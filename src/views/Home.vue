2711d50bd76689177ba3ef6c8cbba100a521a112
Josee <info@joseewouters.nl>
2021-10-29 23:36:12 +0200

    Add home with date and time fields

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <form>
+        <fieldset>
+          <label for="date">Date</label>
+          <input type="date" name="date" id="date">
+        </fieldset>
+        <fieldset>
+          <label for="time">Time</label>
+          <input type="time" name="time" id="time">
+        </fieldset>
+      </form>
+      <ion-button>Add new entry</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+
+export default  {
+  name: 'Home',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+}
+</script>
+
+<style scoped>
+fieldset {
+  all: inherit;
+}
+</style>
